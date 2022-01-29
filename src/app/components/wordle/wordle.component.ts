f1512e894c89c18c7d1eaf229bae901a55155efa
cyan92128505 <cyan92128505@gmail.com>
2022-01-30 00:13:19 +0800

    :sparkles: Modify wordle componet

--- src/app/components/wordle/wordle.component.ts
+++ src/app/components/wordle/wordle.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { WordleService } from 'src/app/services/wordle.service';
 @Component({
   selector: 'wordle-component',
   templateUrl: './wordle.component.html',
   styleUrls: ['./wordle.component.scss'],
 })
 export class WordleComponent implements OnInit {
-  constructor() {}
+  wordleResult = 'START';
+
+  constructor(private wordleService: WordleService) {}
 
   ngOnInit(): void {}
+
+  click() {
+    this.wordleResult = this.wordleService.result();
+  }
 }
--- src/app/services/wordle.service.ts
+++ src/app/services/wordle.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { WordleDict } from './wordle.data';
+import { Buffer } from 'buffer';
 
 @Injectable({
   providedIn: 'root',
