aab8ca097cfc4769fa5b21bcaf28d532768145ca
cyan92128505 <cyan92128505@gmail.com>
2022-01-29 23:44:04 +0800

    :sparkles: Add wordle-component

---
+++ src/app/components/wordle/wordle.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wordle-component',
+  templateUrl: './wordle.component.html',
+  styleUrls: ['./wordle.component.scss'],
+})
+export class WordleComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
